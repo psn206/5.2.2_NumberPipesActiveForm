5cca7c13ac564ee05826e141d2dd7cccab41e316
Sergei Prutsakov <psn206@mail.ru>
2024-08-06 16:54:27 +0300

    Добавьте файлы проекта.

---
+++ NumberPipesActiveForm/Main.cs
@@ -0,0 +1,33 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+
+namespace NumberPipesActiveForm
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            List<Element> pipes = new FilteredElementCollector(doc,doc.ActiveView.Id)
+                .OfCategory(BuiltInCategory.OST_PipeCurves)
+                .WhereElementIsNotElementType()
+                .Cast<Element>()
+                .ToList();
+
+            TaskDialog.Show("?", $"Количество воздуховодов= {pipes.Count.ToString()}");
+            return Result.Succeeded;
+        }
+    }
+}
